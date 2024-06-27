fe1dd52bd09503e8333c4baa7e4aafd0d0dab3b1
Dennis <dennis.diaz@epn.edu.ec>
2024-06-27 00:31:45 -0500

    DesarrolloFrame

---
+++ src/main/java/org/example/CalculadoraC.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import javax.swing.*;
+
+public class CalculadoraC {
+    private JTextField Numero1;
+    private JTextField Numero2;
+    private JPanel calculadora;
+    private JButton Suma;
+    private JButton Resta;
+    private JButton Multiplicacion;
+    private JButton division;
+    private JButton potencia;
+    private JButton raiz;
+    private JButton seno;
+    private JButton coseno;
+    private JLabel Resultado;
+    private JLabel Respuesta;
+    private JButton tangente;
+}
