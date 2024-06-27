d8de2f01fe65296e1001dae5a031eea1bb9a59b0
Dennis <dennis.diaz@epn.edu.ec>
2024-06-27 00:42:37 -0500

    DesarrolloDeSenCosTan

--- src/main/java/org/example/CalculadoraC.java
+++ src/main/java/org/example/CalculadoraC.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import java.lang.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class CalculadoraC {
     private JTextField Numero1;
     private JTextField Numero2;
-    private JPanel calculadora;
+    public JPanel calculadora;
     private JButton Suma;
     private JButton Resta;
     private JButton Multiplicacion;
@@ -17,4 +21,42 @@ public class CalculadoraC {
     private JLabel Resultado;
     private JLabel Respuesta;
     private JButton tangente;
+
+
+    public CalculadoraC() {
+        seno.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero2 = Numero2.getText();
+                double num2=new Double(numero2).doubleValue();
+                Numero1.setText("Sen()");
+                double seno = Math.sin(num2);
+                String senoStr = String.format("%.2f", seno);
+                Respuesta.setText(String.valueOf(senoStr));
+            }
+        });
+
+        coseno.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero2 = Numero2.getText();
+                double num2=new Double(numero2).doubleValue();
+                Numero1.setText("Cos()");
+                double coseno = Math.cos(num2);
+                String cosenoStr = String.format("%.2f", coseno);
+                Respuesta.setText(String.valueOf(cosenoStr));
+            }
+        });
+        tangente.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero2 = Numero2.getText();
+                double num2=new Double(numero2).doubleValue();
+                Numero1.setText("Tan()");
+                double tangente = Math.tan(num2);
+                String tangenteStr = String.format("%.2f", tangente);
+                Respuesta.setText(String.valueOf(tangenteStr));
+            }
+        });
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("Mi aplicacion de login"); //Intanciado
+        frame.setContentPane(new CalculadoraC().calculadora);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Cerrar procesos
+        frame.setSize(20000, 1000);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
