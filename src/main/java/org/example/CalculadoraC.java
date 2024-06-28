92d6de0e7b5caa634cb3ff6eccf81dc65a76e556
Wilmer Vargas <wilmer.vargas@epn.edu.ec>
2024-06-28 12:53:14 -0500

    Primer cambio

--- src/main/java/org/example/CalculadoraC.java
+++ src/main/java/org/example/CalculadoraC.java
@@ -24,6 +24,86 @@ public class CalculadoraC {
 
 
     public CalculadoraC() {
+
+        //operciones Basicas
+        Suma.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero1 = Numero1.getText();
+                String numero2 = Numero2.getText();
+                double num1 = Double.parseDouble(numero1);
+                double num2 = Double.parseDouble(numero2);
+                double suma = num1 + num2;
+                Respuesta.setText(String.format("%.2f", suma));
+
+            }
+        });
+        Resta.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero1 = Numero1.getText();
+                String numero2 = Numero2.getText();
+                double num1 = Double.parseDouble(numero1);
+                double num2 = Double.parseDouble(numero2);
+                double resta = num1 - num2;
+                Respuesta.setText(String.format("%.2f", resta));
+
+            }
+        });
+        Multiplicacion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero1 = Numero1.getText();
+                String numero2 = Numero2.getText();
+                double num1 = Double.parseDouble(numero1);
+                double num2 = Double.parseDouble(numero2);
+                double multiplicacion = num1 * num2;
+                Respuesta.setText(String.format("%.2f", multiplicacion));
+
+            }
+        });
+        division.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero1 = Numero1.getText();
+                String numero2 = Numero2.getText();
+                double num1 = Double.parseDouble(numero1);
+                double num2 = Double.parseDouble(numero2);
+                if (num2 == 0) {
+                    Respuesta.setText("Imposible");
+                    JOptionPane.showMessageDialog(null, "División por cero no es posible.");
+                } else {
+                    double div = num1 / num2;
+                    Respuesta.setText(String.format("%.2f", div));
+                }
+
+
+            }
+        });
+        potencia.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero1 = Numero1.getText();
+                String numero2 = Numero2.getText();
+                double num1 = Double.parseDouble(numero1);
+                double num2 = Double.parseDouble(numero2);
+                double potencia = Math.pow(num1, num2);
+                Respuesta.setText(String.format("%.2f", potencia));
+
+            }
+        });
+        raiz.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String numero2 = Numero2.getText();
+                double num2 = Double.parseDouble(numero2);
+                Numero1.setText("Raiz-cuadrada()");
+                double raizCuadrada = Math.sqrt(num2);
+                Respuesta.setText(String.format("%.2f", raizCuadrada));
+
+            }
+        });
+
         seno.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -58,5 +138,6 @@ public class CalculadoraC {
                 Respuesta.setText(String.valueOf(tangenteStr));
             }
         });
+
     }
 }
